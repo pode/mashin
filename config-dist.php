d90a69d169fce6f1f8e438bc84a8f9f54e9d41eb
Magnus Enger <magnus@torfeus.deich.folkebibl.no>
2009-11-20 10:54:55 +0100

    Flytter config.php til config-dist.php

---
+++ config-dist.php
@@ -0,0 +1,16 @@
+<?php
+
+// Configuration file for "mashin"
+
+// Base URL for SRU
+// Change this to the base URL of your own installation
+$config['sru'] = 'http://torfeus.deich.folkebibl.no:9999/biblios';
+
+// Last.fm
+// Get your own API key here: http://www.last.fm/api
+$config['lastfm'] = array(
+	'api_key' => 'YOUR API KEY', 
+	'api_root' => 'http://ws.audioscrobbler.com/2.0/'
+);
+
+?>
