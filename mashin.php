d91cc038ffa9faa903099a375f711c2f7a3e1ee5
Magnus Enger <magnus@torfeus.deich.folkebibl.no>
2009-11-20 11:03:50 +0100

    Legger til lisens i alle filer

--- mashin.php
+++ mashin.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+
+Copyright 2009 ABM-utvikling
+
+This file is part of "Podes mashin".
+
+"Podes mashin" is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published bythe Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ "Podes mashin" is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with "Podes mashin". If not, see <http://www.gnu.org/licenses/>.
+        
+Source code available from:
+http://github.com/pode/mashin/
+                        
+This code is intended to be used with the open source ILS Koha.
+
+*/
+
 if (!empty($_GET['id'])) {
 
 	include_once('config.php');
